aa4a092d3f36ea9336ac5b453ba74aed0e0dc9ba
Dylan89262 <dylan89262@students.sbcsc.k12.in.us>
2023-01-13 13:38:04 -0500

    Finsished code

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -8,8 +8,19 @@ public class LetterFrequencies
 		String letters = "abcdefghijklmnopqrstuvwxyz"; 
 		String phrase = "This is a phrase!";
 
-		System.out.println("The following shows the letter frequencies for the phrase");
+		System.out.println("The following shows the letter frequencies for the phrase\n");
 
     /* your code here */
+	for (int i = 0; i < 26; i++){
+		String Alphchar = letters.substring(i, i+1);
+		int count = 0;
+		for (int c = 0; c < phrase.length(); c++){
+			String Phrasechar = phrase.substring(c, c+1);
+			if (Alphchar.equals(Phrasechar)){
+				count++;
+			}
+		}
+		System.out.println(Alphchar + " appeared " + count + " times");
+	}
 	}
 }
